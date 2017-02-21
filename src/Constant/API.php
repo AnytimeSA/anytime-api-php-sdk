54f5dfdb9ea1b009c6bc9b4376f60e55d05b9ab1
dve <david.vanderelst@anyti.me>
2017-02-21 19:07:51 +0100

    Add all API constants

--- src/Constant/API.php
+++ src/Constant/API.php
@@ -4,5 +4,24 @@ namespace Anytime\ApiClient\Constant;
 
 class API
 {
+    const OAUTH2_TOKEN = 'OAuth2Token';
     const APICHECK = 'ApiCheck';
+    const APICHECK_POST = 'ApiCheckPost';
+    const ACCOUNT_LIST = 'AccountList';
+    const ACCOUNT_INFO = 'AccountInfo';
+    const ACCOUNT_CREATION = 'AccountCreation';
+    const ACCOUNT_UPDATE = 'AccountUpdate';
+    const ACCOUNT_CREDIT_TRANSFER = 'AccountCreditTransfer';
+    const ACCOUNT_BALANCE = 'AccountBalance';
+    const ACCOUNT_STATEMENT = 'AccountStatement';
+    const ACCOUNT_KYC = 'AccountKyc';
+    const ACCOUNT_CARD_LIST = 'AccountCardList';
+    const CARD_INFO = 'CardInfo';
+    const CARD_ORDER = 'CardOrder';
+    const CARD_UPDATE = 'CardUpdate';
+    const CARD_STATEMENT = 'CardStatement';
+    const CARD_CREDIT = 'CardCredit';
+    const CARD_DEBIT = 'CardDebit';
+    const CARD_SEND_PIN = 'CardSendPin';
+    const CARD_DETAILS_DISPLAY = 'CardDetailsDisplay';
 }
