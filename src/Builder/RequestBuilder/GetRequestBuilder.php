73fad2acc6877192c8cc66a505c9e481948f208a
dve <david.vanderelst@anyti.me>
2017-02-24 11:42:24 +0100

    Create getQueryStringFilters in the GetRequestBuilder class to create query string based on existing parameters supposed to be in the query string (date / limit...)

--- src/Builder/RequestBuilder/GetRequestBuilder.php
+++ src/Builder/RequestBuilder/GetRequestBuilder.php
@@ -15,7 +15,44 @@ abstract class GetRequestBuilder extends RequestBuilder
     {
         return $this->createRequestObject(
             $modelRequest->getMethod(),
-            $this->getFullUrl($this->preBuiltData['uri'])
+            $this->getFullUrl($this->preBuiltData['uri']) . $this->getQueryStringFilters($modelRequest)
         );
+    }
+
+    /**
+     * @param ModelRequest $modelRequest
+     * @return string
+     */
+    public function getQueryStringFilters(ModelRequest $modelRequest)
+    {
+        $output = [];
+        $mappings = [
+            'getDateStart'      =>  'date_start',
+            'getDateEnd'        =>  'date_end',
+            'getLimitStart'     =>  'limit_start',
+            'getLimitNumber'    =>  'limit_number'
+        ];
+
+        foreach($mappings as $methodName => $paramName) {
+            if(method_exists($modelRequest, $methodName) && !is_null($paramName)) {
+                $value = $modelRequest->$methodName();
+                if(is_object($value)) {
+                    if(get_class($value) === 'DateTime') {
+                        $value = $value->format('Y-m-d');
+                    } else {
+                        $value = '';
+                    }
+                }
+                $output[$paramName] = $value;
+            }
+        }
+
+        $query = http_build_query($output, '', '&');
+
+        if(!empty($query)) {
+            return '?' . $query;
+        }
+
+        return '';
     }
 }
