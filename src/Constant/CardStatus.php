fc5d413ae3f9c003a8a6b2cf1ddadefc40d4c8a6
dve <david.vanderelst@anyti.me>
2017-02-27 12:40:11 +0100

    Add CardStatus constants class

---
+++ src/Constant/CardStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Anytime\ApiClient\Constant;
+
+class CardStatus
+{
+    const STOLEN        =   'STO';
+    const LOST          =   'LOS';
+    const EXPIRED       =   'EXP';
+    const DAMAGED       =   'DAM';
+    const CANCELED      =   'CAN';
+    const ACTIVATED     =   'ACT';
+    const BLOCKED       =   'BLO';
+    const PRE_ORDERED   =   'PRE';
+    const ORDERED       =   'ORD';
+}
