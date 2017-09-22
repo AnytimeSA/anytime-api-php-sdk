147d3b81a47fb01a190879f3bab49cfa0d2725d9
dve <david.vanderelst@anyti.me>
2017-09-22 18:42:21 +0200

    Fix an issue in the card update request building making the request failed for virtual cards

--- src/Builder/RequestBuilder/PutCardUpdateRequestBuilder.php
+++ src/Builder/RequestBuilder/PutCardUpdateRequestBuilder.php
@@ -18,7 +18,6 @@ class PutCardUpdateRequestBuilder extends RequestBuilder
             $modelRequest->getMethod(),
             $this->getFullUrl($this->preBuiltData['uri']),
             $this->filterNullParams([
-                'card_ref'      => $modelRequest->getCardRef(),
                 'card_status'   => $modelRequest->getCardStatus(),
                 'card_pos'      => $modelRequest->getCardPos(),
                 'card_atm'      => $modelRequest->getCardAtm(),
